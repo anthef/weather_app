37bbf996ca82412c05ab3da6127cb2330a20a794
anthef <anthonyef09@gmail.com>
2024-06-10 14:24:42 +0700

    next one tapi masih belum rapih

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,81 +15,216 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        systemOverlayStyle: const SystemUiOverlayStyle(
-          statusBarBrightness: Brightness.dark
-          ),
+        systemOverlayStyle:
+            const SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
       ),
-      body:Padding(
-        padding: const EdgeInsets.fromLTRB(40, 1.2*kToolbarHeight, 40, 20),
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: Stack(
-            children: [
-              Align(
-                alignment: AlignmentDirectional(3, -0.3),
-                child: Container(
-                  height: 300,
-                  width: 300,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors. deepPurple
+      body: Padding(
+          padding: const EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
+          child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: AlignmentDirectional(3, -0.3),
+                    child: Container(
+                      height: 300,
+                      width: 300,
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle, color: Colors.deepPurple),
+                    ),
                   ),
-                ),
-                ),
-
-              Align(
-                alignment: const AlignmentDirectional(-3, -0.3),
-                child: Container(
-                  height: 300,
-                  width: 300,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors. deepPurple
+                  Align(
+                    alignment: const AlignmentDirectional(-3, -0.3),
+                    child: Container(
+                      height: 300,
+                      width: 300,
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle, color: Colors.deepPurple),
+                    ),
                   ),
-                ),
-              ),
-
-              Align(
-                alignment: const AlignmentDirectional(0, -1.2),
-                child: Container(
-                  height: 300,
-                  width: 600,
-                  decoration: const BoxDecoration(
-                    color: Color(0xFFFFAB40),
-                  ),
-                ),
-              ),
-
-              BackdropFilter(
-                filter: ImageFilter.blur(sigmaX:100, sigmaY:100),
-                child: Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.transparent
-                  ),
-                )
-              ),
-
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                child: const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '📍 Aix-en-Provence',
-                      style : TextStyle(
-                        color:Colors.white,
-                        fontWeight: FontWeight.w300
+                  Align(
+                    alignment: const AlignmentDirectional(0, -1.2),
+                    child: Container(
+                      height: 300,
+                      width: 600,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFFFFAB40),
                       ),
                     ),
-                    
+                  ),
+                  BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
+                      child: Container(
+                        decoration:
+                            const BoxDecoration(color: Colors.transparent),
+                      )),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          '📍Aix-en-Provence',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.w300),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          'Good Morning',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25),
+                        ),
+                        Image.asset('lib/images/1.png'),
+                        const Center(
+                          child: Text(
+                            '21°C',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 55,
+                                fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                        const Center(
+                          child: Text(
+                            'THUNDERSTORM',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        const Center(
+                          child: Text(
+                            'Monday 10 · 13.52pm',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w300),
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset('lib/images/9.png', scale: 8),
+                                const SizedBox(width: 5),
+                                const Column(
+                                  children: [
+                                    Text(
+                                      'Sunrise',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w300),
+                                    ),
+                                    SizedBox(height: 3,),
+                                    Text(
+                                      '5:34 am',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w700),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset('lib/images/10.png', scale: 8),
+                                const SizedBox(width: 5),
+                                const Column(
+                                  children: [
+                                    Text(
+                                      'Sunset',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w300),
+                                    ),
+                                    SizedBox(height: 3,),
+                                    Text(
+                                      '7:34 pm',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w700),
+                                    )
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                        const Padding(
+                          padding:EdgeInsets.symmetric(vertical: 5),
+                          child: Divider(
+                            color: Colors.grey,
+                            ),
+                          ),
+                        
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset('lib/images/11.png', scale: 8),
+                                const SizedBox(width: 5),
+                                const Column(
+                                  children: [
+                                    Text(
+                                      'Temp Max',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w300),
+                                    ),
+                                    SizedBox(height: 3,),
+                                    Text(
+                                      '36°C',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w700),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset('lib/images/12.png', scale: 8),
+                                const SizedBox(width: 5),
+                                const Column(
+                                  children: [
+                                    Text(
+                                      'Temp Min',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w300),
+                                    ),
+                                    SizedBox(height: 3,),
+                                    Text(
+                                      '19°C',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w700),
+                                    )
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
-                ),
-              ),
-            ],
-          )
-        )
-        ),
+              ))),
     );
   }
 }
